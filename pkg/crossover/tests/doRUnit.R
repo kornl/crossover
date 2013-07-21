820a935e791f354e6328ace53448507c07b0274c
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2013-07-21 09:48:21 +0200

    Loading library MASS and multcomp in RUnit tests since they are no longer a dependency.

--- pkg/crossover/tests/doRUnit.R
+++ pkg/crossover/tests/doRUnit.R
@@ -3,7 +3,8 @@
 if(require("RUnit", quietly=TRUE)) {
 	
 	## --- Setup ---
-	
+    library(MASS)
+    library(multcomp)
 	pkg <- "crossover" 
 	if(Sys.getenv("RCMDCHECK") == "FALSE") {
 		## Path to unit tests for standalone running under Makefile (not R CMD check)
