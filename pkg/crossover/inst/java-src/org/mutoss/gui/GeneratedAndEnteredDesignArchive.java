c7ad8a5c0c410a50f66dd29d7b03a91776af0fb6
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2013-08-13 14:56:16 +0200

    Class to save designs which were found by the algorithm or entered by the user.

---
+++ pkg/crossover/inst/java-src/org/mutoss/gui/GeneratedAndEnteredDesignArchive.java
@@ -0,0 +1,14 @@
+package org.mutoss.gui;
+
+public class GeneratedAndEnteredDesignArchive {
+	
+	public GeneratedAndEnteredDesignArchive() {
+		
+	}
+	
+	public void addDesign(Design design) {
+		
+	}
+	
+	
+}
