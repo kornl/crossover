0eb099d42b4360857a41bfe1a63e0f3619510a92
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2013-08-17 23:08:54 +0200

    general.carryover takes model from crossoverSearchResult if missing.

--- pkg/crossover/R/general.carryover.R
+++ pkg/crossover/R/general.carryover.R
@@ -29,7 +29,10 @@
 #' 
 #' @export general.carryover
 general.carryover<-function(design,model,t0=1,rho=0.5){  
-  if (class(design)=="crossoverSearchResult") design <- design@design
+  if (class(design)=="crossoverSearchResult") {
+      if(missing(model)) model <- design@model
+      design <- design@design      
+  }
   if (class(design)=="crossoverDesign") {
     model <- design@model
     design <- design@design
