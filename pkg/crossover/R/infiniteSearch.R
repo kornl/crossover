c748b4f6e7dc67763c187560ed839e5500c44454
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2013-08-21 22:29:42 +0200

    R code for the in(de)finite search.

---
+++ pkg/crossover/R/infiniteSearch.R
@@ -0,0 +1,20 @@
+infiniteSearch <- function(oldResult=NULL, ..., start.designs) {
+    if (is.null(oldResult)) {
+        result <- searchCrossOverDesign(start.designs=start.designs, ...)
+        return(result)
+    }
+    result <- searchCrossOverDesign(start.designs=list(getDesign(oldResult)), ...)
+    return(combineResults(oldResult, result))
+}
+
+combineResults <- function(x,y) {    
+    return(new("crossoverSearchResult", 
+               design=y@design, 
+               startDesigns=c(x@startDesigns, y@startDesigns), 
+               eff=c(x@eff, y@eff),                   
+               search=x@search, 
+               model=c(x@model, y@model), 
+               time=c(x@time, y@time), 
+               misc=c(x@misc, y@misc)))
+    
+}
