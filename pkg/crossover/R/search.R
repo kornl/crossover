581ae409bf9633d0c94f186775192c6c1e573a48
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2013-08-22 23:31:25 +0200

    Moving estimable-functions to their own file and removing a duplicate.

--- pkg/crossover/R/search.R
+++ pkg/crossover/R/search.R
@@ -317,40 +317,6 @@ randomDesignWithoutCheck <- function(s, p, v,  v.rep, balance.s=FALSE, balance.p
     return(design)
 }
 
-estimable_R <- function(design, v, model, C, verbose=0) {
-  if(missing(C)) {
-    Csub <- contrMat(n=rep(1, v), type="Tukey")
-    class(Csub) <- "matrix" #TODO Package matrix can be improved here (IMO)!
-    C <- appendZeroColumns(Csub, model, v)
-  }
-  rcDesign <- rcd(design, v=v, model=model)
-  Xr <- rcdMatrix(rcDesign, v, model)
-  H <- linkMatrix(model, v)
-  X <- Xr %*% H
-  Z <- getZ(s=dim(design)[2],p=dim(design)[1])
-  X <- cbind(X, Z)  
-  XX <- t(X) %*% X
-  C <- cbind(C, matrix(0, dim(C)[1], dim(Z)[2]))
-  if (verbose) {            
-      print(rcDesign)
-      print(Xr)
-      print(abs(C %*% ginv(XX) %*% XX-C))
-  }
-  return(isTRUE(all.equal(C %*% ginv(XX) %*% XX, C, check.attributes=FALSE, check.names=FALSE))) # Criterion to test whether - see Theorem \ref{thr:estimable} of vignette.
-}
-
-estimable <- function(design, v, model, C, verbose=0) {
-    if(missing(C)) {
-        Csub <- contrMat(n=rep(1, v), type="Tukey")
-        class(Csub) <- "matrix" #TODO Package matrix can be improved here (IMO)!
-        C <- appendZeroColumns(Csub, model, v)
-    }
-    rcDesign <- rcd(design, v=v, model=model)
-    linkM <- linkMatrix(model, v)
-    Z <- getZ(s=dim(design)[2],p=dim(design)[1])
-    return(.Call( "estimable2R", rcDesign, v, model, linkM, C, Z, verbose, PACKAGE = "crossover" ))    
-}
-
 getS1 <- function(design, v, model, C, verbose=0) {
     if(missing(C)) {
         Csub <- contrMat(n=rep(1, v), type="Tukey")
