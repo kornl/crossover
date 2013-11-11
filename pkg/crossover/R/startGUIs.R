b5ff6a39d2c3728f68729b72182f2f4ef5e26aaf
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2013-11-11 01:04:14 +0100

    There is now a different function in startGUIs.R for getTable.

--- pkg/crossover/R/startGUIs.R
+++ pkg/crossover/R/startGUIs.R
@@ -22,3 +22,48 @@
 crossoverGUI <- function() {
 	invisible(.jnew("org/mutoss/gui/CrossoverGUI"))	
 }
+
+getTable <- function(d, type="HTML") {
+  d <- design2integer(d)
+  rownames(d) <- paste("p", 1:dim(d)[1], sep="")
+  colnames(d) <- paste("s", 1:dim(d)[2], sep="")
+  if (type=="ASCII") {
+    return(paste("<pre>",paste(capture.output(print(d)), collapse="\n"),"</pre>"))
+  } else if (type=="HTML") {
+    return(print(xtable(d), type="html"))
+  } else if (type=="R") {
+    return(paste("<pre>",dputMatrix(d),"</pre>"))
+  }
+}
+
+dputMatrix <- function(m, name, indent=6, rowNames=FALSE) {
+  s <- "rbind("
+  if (!missing(name)) s <- paste(name,"<- rbind(") 
+  for (i in 1:(dim(m)[1])) {
+    nameLater <- FALSE
+    if (any(make.names(row.names(m))!=row.names(m))) {
+      rowNames <- FALSE
+      nameLater <- TRUE
+    }
+    rName <- ifelse(rowNames, paste(row.names(m)[i],"=",sep=""), "")
+    s <- paste(s, 
+               ifelse(i==1,"",paste(rep(" ",indent),collapse="")),
+               rName,
+               dputS(unname(m[i,])),
+               ifelse(i==dim(m)[1],")\n",",\n"),
+               sep="")            
+  }
+  if (nameLater) {
+    if (missing(name)) {
+      warning("Can set row names if no name for matrix is given.")
+      return(s)
+    }
+    s <- paste(s, 
+               "row.names(",name,") <- ", dputS(row.names(m)), "\n", sep="")
+  }
+  return(s)
+}
+
+dputS <- function(x) {
+  paste(capture.output(dput(x)), collapse="\n")
+}
