0937eadf5f6c26ed3a68780d68202ac6a1a0c2d7
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2014-01-12 02:53:55 +0100

    Transpose designs and save them all in 'clatworthy1.rda'.

--- build/readInClatworthy.R
+++ build/readInClatworthy.R
@@ -12,20 +12,14 @@ for (f in dir()) {
   p <- dim(t)[2]
   attr(t, "signature") <- paste("p = ",p,", n = ",s,", t = ",v,sep="")
   designName <- tolower(make.names(paste("CLATWORTHY",gsub(".DAT","", f))))
-  assign(designName, t)
+  assign(designName, t(t))
   
   con <- textConnection(data[c(2)])    
   meta <- as.matrix(read.table(con, header = FALSE))[1,]
   if (meta[1]!=v) cat("v differs for",designName,": ", meta[1], v, "\n")
   if (meta[2]!=s) cat("s differs for",designName,": ", meta[2], s, "\n")
-  if (meta[3]!=p) cat("ü differs for",designName,": ", meta[3], p, "\n")
+  if (meta[3]!=p) cat("p differs for",designName,": ", meta[3], p, "\n")
 }
 
-
-
-
-davisHall6ta <- t
-
-
-save(davisHall6ta, davisHall6tb, davisHall6tc, davisHall7ta, davisHall7tb, davisHall7tc, davisHall8ta, davisHall8tb, davisHall8tc, davisHall9ta, davisHall9tb, davisHall9tc, file="davisHall.rda")
+save(list=ls(pattern="clatworthy*"), file="../clatworthy1.rda")
 
