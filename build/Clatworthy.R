0fcd3cd7c4a6d725b7cb5fa117641ded3a561b2d
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2014-10-17 19:50:49 +0200

    Code to find balanced designs.

--- build/Clatworthy.R
+++ build/Clatworthy.R
@@ -11,3 +11,26 @@ for (file in dir(path=path)) {
     }
   }
 }
+
+path <- system.file("data", package="Crossover")
+balancedDesigns1 <- c()
+balancedDesigns2 <- c()
+for (file in dir(path=path)) {  	 
+  designs <- load(paste(path, file, sep="/"))
+  for (designS in designs) {
+    design <- get(designS)
+    p <- dim(design)[1]
+    s <- dim(design)[2]
+    v <- length(levels(as.factor(design)))			
+    variances <- general.carryover(design, model=1)$Var.trt.pair[upper.tri(diag(v))]
+    if(isTRUE(all.equal(min(variances), max(variances)))) {
+      balancedDesigns1 <- c(balancedDesigns1, designS)
+      cat(designS, " is balanced!\n")
+    }
+    if (max(variances)-min(variances)<0.001 && !isTRUE(all.equal(min(variances), max(variances)))) {
+      balancedDesigns2 <- c(balancedDesigns2, designS)
+      cat(designS, " is nearly balanced (", max(variances)-min(variances) ,")!\n")
+    }
+  }
+}
+
