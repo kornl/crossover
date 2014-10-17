789625185aa3081277f9f9926860c83af8148508
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2014-10-17 21:12:30 +0200

    Combinations with all balanced designs.

--- build/Clatworthy.R
+++ build/Clatworthy.R
@@ -12,10 +12,12 @@ for (file in dir(path=path)) {
   }
 }
 
+library(Crossover)
 path <- system.file("data", package="Crossover")
 balancedDesigns1 <- c()
 balancedDesigns2 <- c()
 for (file in dir(path=path)) {  	 
+  if (file %in% c("clatworthy1.rda", "clatworthyC.rda", "pbib2combine.rda")) next
   designs <- load(paste(path, file, sep="/"))
   for (designS in designs) {
     design <- get(designS)
@@ -34,3 +36,46 @@ for (file in dir(path=path)) {
   }
 }
 
+# Result 17.10.2014:
+balancedDesigns1 <- c("andersonPreece", "anderson", "archdeacon", "atkinson3t", "atkinson4t", 
+                      "atkinson5t", "balaam3t", "balaam4t", "balaam5t", "balaam6t", 
+                      "berenblut3t", "berenblut4t", "berenblut5t", "davisHall7tb", 
+                      "federerAtkinson3ta", "federerAtkinson3tb", "federerAtkinson4ta", 
+                      "federerAtkinson4tb", "federerAtkinson5ta", "fletcher9", "iqbalJones1", 
+                      "iqbalJones2", "iqbalJones3", "iqbalJones4", "iqbalJones5", "iqbalJones7", 
+                      "iqbalJones11", "iqbalJones17", "iqbalJones23", "lewisFletcherMatthews3", 
+                      "orthogonalLatinSquare3t", "orthogonalLatinSquare4t", "orthogonalLatinSquare5t", 
+                      "orthogonalLatinSquare7t", "pattersonLucasExtraPeriod30", "pattersonLucasExtraPeriod31", 
+                      "pattersonLucasExtraPeriod32", "pattersonLucasExtraPeriod33", 
+                      "pattersonLucasExtraPeriod34", "pattersonLucasExtraPeriod35", 
+                      "pattersonLucasExtraPeriod36", "pattersonLucasExtraPeriod37", 
+                      "pattersonLucasExtraPeriod38", "pattersonLucasExtraPeriod39", 
+                      "pattersonLucasExtraPeriod40", "pattersonLucasExtraPeriod41", 
+                      "pattersonLucasExtraPeriod42", "pattersonLucasExtraPeriod43", 
+                      "pattersonLucasExtraPeriod44", "pattersonLucasExtraPeriod45", 
+                      "pattersonLucasExtraPeriod46", "pattersonLucasExtraPeriod47", 
+                      "pattersonLucasExtraPeriod48", "pattersonLucasExtraPeriod49", 
+                      "pattersonLucasExtraPeriod86", "pattersonLucasPltT1", "pattersonLucasPltT3", 
+                      "pattersonLucasPltT4", "pattersonLucasPltT5", "pattersonLucasPltT7", 
+                      "pattersonLucasPltT8", "pattersonLucasPltT9", "pattersonLucasPltT10", 
+                      "pattersonLucasPltT12", "pattersonLucasPltT13", "pattersonLucasPltT15", 
+                      "pattersonLucasPltT16", "pattersonLucasPltT17", "pattersonLucasPltT18", 
+                      "pattersonLucasPltT19", "pattersonLucasPltT20", "pattersonLucasPltT21", 
+                      "pattersonLucasPltT22", "pattersonLucasPltT23", "prescott1", 
+                      "prescott2", "quenouille3t2", "quenouille4t1", "quenouille4t2", 
+                      "quenouille4t3", "switchback3t", "switchback4t", "switchback5t", 
+                      "switchback6t", "switchback7t", "williams3t", "williams4t", "williams5t", 
+                      "williams6t", "williams7t", "williams8t", "williams9t")
+balancedDesigns2 <- c("bateJones5t", "bateJones8t", "iqbalJones31", "quenouille3t1")
+balancedDesigns <- c(balancedDesigns1, balancedDesigns2)
+
+# Show all designs in the data files.
+path <- system.file("data", package="Crossover")
+balancedDesigns1 <- c()
+balancedDesigns2 <- c()
+for (file in dir(path=path)) {     
+  #if (file %in% c("clatworthy1.rda", "clatworthyC.rda")) next
+  cat(file, "\n")
+  designs <- load(paste(path, file, sep="/"))
+  print(designs)
+}
--- pkg/Crossover/R/misc.R
+++ pkg/Crossover/R/misc.R
@@ -15,13 +15,47 @@ subs.treatment <- function(design, new.treatments) {
 # coDesign <- get("williams4t", Crossover:::Crossover.env)
 
 combine.PBIB <- function() {
+  
+  balancedDesigns1 <- c("andersonPreece", "anderson", "archdeacon", "atkinson3t", "atkinson4t", 
+                        "atkinson5t", "balaam3t", "balaam4t", "balaam5t", "balaam6t", 
+                        "berenblut3t", "berenblut4t", "berenblut5t", "davisHall7tb", 
+                        "federerAtkinson3ta", "federerAtkinson3tb", "federerAtkinson4ta", 
+                        "federerAtkinson4tb", "federerAtkinson5ta", "fletcher9", "iqbalJones1", 
+                        "iqbalJones2", "iqbalJones3", "iqbalJones4", "iqbalJones5", "iqbalJones7", 
+                        "iqbalJones11", "iqbalJones17", "iqbalJones23", "lewisFletcherMatthews3", 
+                        "orthogonalLatinSquare3t", "orthogonalLatinSquare4t", "orthogonalLatinSquare5t", 
+                        "orthogonalLatinSquare7t", "pattersonLucasExtraPeriod30", "pattersonLucasExtraPeriod31", 
+                        "pattersonLucasExtraPeriod32", "pattersonLucasExtraPeriod33", 
+                        "pattersonLucasExtraPeriod34", "pattersonLucasExtraPeriod35", 
+                        "pattersonLucasExtraPeriod36", "pattersonLucasExtraPeriod37", 
+                        "pattersonLucasExtraPeriod38", "pattersonLucasExtraPeriod39", 
+                        "pattersonLucasExtraPeriod40", "pattersonLucasExtraPeriod41", 
+                        "pattersonLucasExtraPeriod42", "pattersonLucasExtraPeriod43", 
+                        "pattersonLucasExtraPeriod44", "pattersonLucasExtraPeriod45", 
+                        "pattersonLucasExtraPeriod46", "pattersonLucasExtraPeriod47", 
+                        "pattersonLucasExtraPeriod48", "pattersonLucasExtraPeriod49", 
+                        "pattersonLucasExtraPeriod86", "pattersonLucasPltT1", "pattersonLucasPltT3", 
+                        "pattersonLucasPltT4", "pattersonLucasPltT5", "pattersonLucasPltT7", 
+                        "pattersonLucasPltT8", "pattersonLucasPltT9", "pattersonLucasPltT10", 
+                        "pattersonLucasPltT12", "pattersonLucasPltT13", "pattersonLucasPltT15", 
+                        "pattersonLucasPltT16", "pattersonLucasPltT17", "pattersonLucasPltT18", 
+                        "pattersonLucasPltT19", "pattersonLucasPltT20", "pattersonLucasPltT21", 
+                        "pattersonLucasPltT22", "pattersonLucasPltT23", "prescott1", 
+                        "prescott2", "quenouille3t2", "quenouille4t1", "quenouille4t2", 
+                        "quenouille4t3", "switchback3t", "switchback4t", "switchback5t", 
+                        "switchback6t", "switchback7t", "williams3t", "williams4t", "williams5t", 
+                        "williams6t", "williams7t", "williams8t", "williams9t")
+  balancedDesigns2 <- c("bateJones5t", "bateJones8t", "iqbalJones31", "quenouille3t1")
+  balancedDesigns <- c(balancedDesigns1, balancedDesigns2)
+  
   # List of PBIB(2):
   # path <- system.file("data", package="Crossover")
   x <- load("pkg/Crossover/data/clatworthy1.rda")
   designs <- c()
   # Available crossover designs:
   summary <- buildSummaryTable()
-  summary <- summary[grep("williams", summary$dataset), ]
+  #summary <- summary[grep("williams", summary$dataset), ]
+  summary <- summary[summary$dataset %in% balancedDesigns, ]
   for (pbib.name in x) {
     pbib <- get(pbib.name)
     t.per.block <- dim(pbib)[1] # verified with book
@@ -33,13 +67,13 @@ combine.PBIB <- function() {
         coDesign <- get(coDesigns$dataset[row], envir=Crossover:::Crossover.env)
         coTitle <- coDesigns[row, "title"]
         coReference <- coDesigns[row, "reference"]
-        title <- paste(paste("PB2.", tail(strsplit(pbib.title, " ")[[1]], 1), sep=""), "- Design from combining PBIB(2)", pbib.title, "with", coTitle)
+        title <- paste(paste("PB2.", tail(strsplit(pbib.title, " ")[[1]], 1), "-",  coDesigns$dataset[row], sep=""), "- Design from combining PBIB(2)", pbib.title, "with", coTitle)
         cat("Creating design ", title, "\n")
         design <- c()
         for (col in 1:(dim(pbib)[2])) {
           design <- cbind(design, subs.treatment(coDesign, pbib[, col]))
         }
-        print(design)
+        #print(design)
         design.name <- tolower(paste("pb2.", coDesigns$dataset[row], ".", tail(strsplit(pbib.title, " ")[[1]], 1), sep=""))
         attr(design, "reference") <- paste(coReference, "Clatworthy, Willard H. Tables of two-associate-class partially balanced designs. US Government Printing Office, 1973.", sep="\n")
         attr(design, "signature") <- paste("p=", dim(design)[1],", n=", dim(design)[2],", t=", max(design), sep="")
