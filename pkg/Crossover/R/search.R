42de5c2db8657e1ef322e1aded07dc31e1044045
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2014-10-20 23:59:37 +0200

    randomDesign gets its own file.

--- pkg/Crossover/R/search.R
+++ pkg/Crossover/R/search.R
@@ -150,32 +150,6 @@ searchCrossOverDesign <- function(s, p, v, model="Standard additive model", eff.
              search=list(n=n, jumps=jumps), model=model, time=time, misc=list(designs=result$designs)))
 }
 
-randomDesign <- function(s, p, v,  v.rep, balance.s=FALSE, balance.p=FALSE, model, C) {
-  if (missing(v.rep)) {
-    v.rep <- rep((s*p) %/% v, v) + c(rep(1, (s*p) %% v), rep(0, v-((s*p) %% v)))
-  }
-  design <- randomDesignWithoutCheck(s, p, v,  v.rep, balance.s, balance.p, model)
-  i <- 0
-  # We should disable the really rare warnings estimable_R could throw.
-  while (!estimable_R(design, v, model, C)) {   
-    i <- i + 1
-    if (i>1000) stop("Could not find design that allows estimation of contrasts after 1000 tries.")
-    design <- randomDesignWithoutCheck(s, p, v,  v.rep, balance.s, balance.p, model)
-  } 
-  return(design)
-}
-
-randomDesignWithoutCheck <- function(s, p, v,  v.rep, balance.s=FALSE, balance.p=FALSE, model) {
-    if (balance.s) {
-        design <- matrix(unlist(tapply(rep(1:v, v.rep), as.factor(rep(1:s,p)), sample)), p, s)
-    } else if (balance.p) {
-        design <- matrix(unlist(tapply(rep(1:v, v.rep), as.factor(rep(1:p,s)), sample)), p, s, byrow=TRUE)
-    } else {
-        design <- matrix(sample(rep(1:v, v.rep)), p, s)
-    }
-    return(design)
-}
-
 getS1 <- function(design, v, model, C, randomS=FALSE, verbose=0) {
     if(missing(C)) {
         Csub <- contrMat(n=rep(1, v), type="Tukey")
